7139bd78ab65a25bef33e6e597753cf22b6bc316
Sajay-P01 <sajaypavi@gmail.com>
2025-09-04 17:39:34 +0530

    FA.v

---
+++ FA.v
@@ -0,0 +1,10 @@
+//carry save adder -- for implementing dadda multiplier
+
+module FA(A,B,Cin,Y,Cout);
+input A,B,Cin;
+output Y,Cout;
+    
+assign Y = A^B^Cin;
+assign Cout = (A&B)|(A&Cin)|(B&Cin);
+    
+endmodule
